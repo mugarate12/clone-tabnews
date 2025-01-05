9c764ce7b6b364e94b94839cbb688f5d0295f83e
Mateus Cardoso <serjumano17@gmail.com>
2025-01-05 11:32:21 -0300

    make database.js more robusto to error handling

--- infra/database.js
+++ infra/database.js
@@ -1,6 +1,6 @@
 import { Client } from "pg";
 
-async function query(queryObject) {
+function getClient() {
   const client = new Client({
     host: process.env.POSTGRES_HOST,
     database: process.env.POSTGRES_DB,
@@ -8,14 +8,50 @@ async function query(queryObject) {
     port: process.env.POSTGRES_PORT,
     password: process.env.POSTGRES_PASSWORD,
   });
+  return client;
+}
+
+async function query(queryObject) {
+  const client = getClient();
   await client.connect();
 
-  const result = await client.query(queryObject);
-  await client.end();
+  try {
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    throw error;
+  } finally {
+    await client.end();
+  }
+};
 
-  return result;
+async function version() {
+  const result = await query('SHOW server_version');
+  return result.rows[0].server_version;
+};
+
+async function maxConnections() {
+  const result = await query('SHOW max_connections');
+  return result.rows[0].max_connections;
+};
+
+async function usedConnections(databaseName = undefined) {
+  if (databaseName === undefined) {
+    databaseName = process.env.POSTGRES_DB;
+  }
+
+  const queryString = 'SELECT COUNT(*) FROM pg_stat_activity WHERE datname = $1;';
+  const result = await query({
+    text: queryString,
+    values: [databaseName],
+  });
+
+  return result.rows[0].count;
 };
 
 export default {
-  query: query
+  query,
+  version,
+  maxConnections,
+  usedConnections,
 };
