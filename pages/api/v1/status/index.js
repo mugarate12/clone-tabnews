dbd93a4087f520035e912345c59c45c579754fd3
Mateus Cardoso <serjumano17@gmail.com>
2025-01-05 11:33:09 -0300

    finish endpoint `/api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,23 @@
 import database from 'infra/database';
 
 async function status(request, response) {
-  const result = await database.query('SELECT 1 + 1 as SUM');
+  const { databaseName } = request.query;
 
-  return response.status(200).json({ status: 'ok' });
+  const updatedAt = new Date().toISOString();
+  const version = await database.version();
+  const maxConnections = await database.maxConnections();
+  const usedConnections = await database.usedConnections(databaseName);
+
+  return response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: version,
+        max_connections: Number(maxConnections),
+        opened_connections: Number(usedConnections),
+      }
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,5 +1,17 @@
 test('Get to /api/v1/status should return 200', async () => {
   const response = await fetch('http://localhost:3000/api/v1/status');
+  const body = await response.json();
+  const updated_at = new Date(body.updated_at).toISOString();
+  console.log(body);
 
   expect(response.status).toBe(200);
+  expect(body.updated_at).toBeDefined();
+  expect(updated_at).toEqual(body.updated_at);
+  expect(body.dependencies.database.version).toBeDefined();
+  expect(body.dependencies.database.version).toBe('16.0');
+  expect(body.dependencies.database.max_connections).toBeDefined();
+  expect(typeof body.dependencies.database.max_connections).toBe('number');
+  expect(body.dependencies.database.opened_connections).toBeDefined();
+  expect(typeof body.dependencies.database.opened_connections).toBe('number');
+  expect(body.dependencies.database.opened_connections).toBe(1);
 });
