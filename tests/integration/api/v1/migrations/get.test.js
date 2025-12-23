76419a2370e2ec9d4d45407f1d0101f3e2711946
Mateus Cardoso <serjumano17@gmail.com>
2025-12-23 14:09:02 -0300

    test: :white_check_mark: rename all descriptions to use new pattern

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -6,11 +6,16 @@ beforeAll(async () => {
   await database.query("DROP SCHEMA public CASCADE; CREATE SCHEMA public;");
 });
 
-test("Get to /api/v1/migrations should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations");
-  const responseBody = await response.json();
+describe('GET /api/v1/migrations', () => {
+  describe('Anonymous user', () => {
+    test("Running pending migrations", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/migrations");
+      const responseBody = await response.json();
 
-  expect(response.status).toBe(200);
-  expect(Array.isArray(responseBody)).toBe(true);
-  expect(responseBody.length).toBeGreaterThan(0);
+      expect(response.status).toBe(200);
+      expect(Array.isArray(responseBody)).toBe(true);
+      expect(responseBody.length).toBeGreaterThan(0);
+    });
+  });
 });
+
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,19 +4,24 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
 
-test("Get to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  const body = await response.json();
-  const updated_at = new Date(body.updated_at).toISOString();
+describe('GET /api/v1/status', () => {
+  describe('Anonymous user', () => {
+    test("Retrieving current system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      const body = await response.json();
+      const updated_at = new Date(body.updated_at).toISOString();
 
-  expect(response.status).toBe(200);
-  expect(body.updated_at).toBeDefined();
-  expect(updated_at).toEqual(body.updated_at);
-  expect(body.dependencies.database.version).toBeDefined();
-  expect(body.dependencies.database.version).toBe("16.0");
-  expect(body.dependencies.database.max_connections).toBeDefined();
-  expect(typeof body.dependencies.database.max_connections).toBe("number");
-  expect(body.dependencies.database.opened_connections).toBeDefined();
-  expect(typeof body.dependencies.database.opened_connections).toBe("number");
-  expect(body.dependencies.database.opened_connections).toBe(1);
+      expect(response.status).toBe(200);
+      expect(body.updated_at).toBeDefined();
+      expect(updated_at).toEqual(body.updated_at);
+      expect(body.dependencies.database.version).toBeDefined();
+      expect(body.dependencies.database.version).toBe("16.0");
+      expect(body.dependencies.database.max_connections).toBeDefined();
+      expect(typeof body.dependencies.database.max_connections).toBe("number");
+      expect(body.dependencies.database.opened_connections).toBeDefined();
+      expect(typeof body.dependencies.database.opened_connections).toBe("number");
+      expect(body.dependencies.database.opened_connections).toBe(1);
+    });
+  });
 });
+
