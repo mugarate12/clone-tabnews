5e04d80e265e9fc5358c6c7af92a26ab5197edee
Mateus Cardoso <serjumano17@gmail.com>
2025-12-26 14:13:45 -0300

    refactor: :recycle: separate updatedat and database info in own components in `/status` page

--- pages/status/index.js
+++ pages/status/index.js
@@ -1,10 +1,16 @@
 import useSWR from "swr";
 
 function StatusPage() {
+  const { data, isLoading } = useSWR("/api/v1/status", fetchAPI, {
+    refreshInterval: 2 * 1000, // 2 seconds
+  });
+
   return (
     <>
-      <h1>Status Page</h1>
-      <UpdatedAt />
+      <h1>Status</h1>
+      <UpdatedAt data={data} isLoading={isLoading} />
+      <h2>Database</h2>
+      <DatabaseInfo data={data} isLoading={isLoading} />
     </>
   )
 }
@@ -17,19 +23,26 @@ async function fetchAPI(key) {
 }
 
 
-function UpdatedAt() {
-  const { data, isLoading } = useSWR("/api/v1/status", fetchAPI, {
-    refreshInterval: 2 * 1000, // 2 seconds
-  });
-
+function UpdatedAt(props) {
   return (
     <div>
-      {isLoading ? "Carregando..." : (
+      {props.isLoading ? "Carregando..." : (
         <>
-          <p>Versão: {data.dependencies.database.version}</p>
-          <p>Conexões abertas: {data.dependencies.database.opened_connections}</p>
-          <p>Máximo de conexões: {data.dependencies.database.max_connections}</p>
-          <p>Última atualização: {new Date(data.updated_at).toLocaleString("pt-BR")}</p>
+          <p>Última atualização: {new Date(props.data.updated_at).toLocaleString("pt-BR")}</p>
+        </>
+      )}
+    </div>
+  );
+}
+
+function DatabaseInfo(props) {
+  return (
+    <div>
+      {props.isLoading ? "Carregando..." : (
+        <>
+          <p>Versão: {props.data.dependencies.database.version}</p>
+          <p>Conexões abertas: {props.data.dependencies.database.opened_connections}</p>
+          <p>Máximo de conexões: {props.data.dependencies.database.max_connections}</p>
         </>
       )}
     </div>
