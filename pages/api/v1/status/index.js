1b91328562e3ccdfd6017f4ba0e1b02ef7bd68ab
Mateus Cardoso <serjumano17@gmail.com>
2025-12-27 12:12:57 -0300

    feat: :sparkles: create and use `InternalServerError` in `/api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,23 +1,34 @@
 import database from "infra/database";
+import { InternalServerError } from "infra/errors";
 
 async function status(request, response) {
-  const { databaseName } = request.query;
+  try {
+    const { databaseName } = request.query;
 
-  const updatedAt = new Date().toISOString();
-  const version = await database.version();
-  const maxConnections = await database.maxConnections();
-  const usedConnections = await database.usedConnections(databaseName);
+    const updatedAt = new Date().toISOString();
+    const version = await database.version();
+    const maxConnections = await database.maxConnections();
+    const usedConnections = await database.usedConnections(databaseName);
 
-  return response.status(200).json({
-    updated_at: updatedAt,
-    dependencies: {
-      database: {
-        version: version,
-        max_connections: Number(maxConnections),
-        opened_connections: Number(usedConnections),
+    return response.status(200).json({
+      updated_at: updatedAt,
+      dependencies: {
+        database: {
+          version: version,
+          max_connections: Number(maxConnections),
+          opened_connections: Number(usedConnections),
+        },
       },
-    },
-  });
+    });
+  } catch (error) {
+    console.log('erro no controller de status');
+    const publicErrorObject = new InternalServerError({
+      cause: error,
+    });
+    console.error(publicErrorObject);
+
+    return response.status(500).json(publicErrorObject);
+  }
 }
 
 export default status;
